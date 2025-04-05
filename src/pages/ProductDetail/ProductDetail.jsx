4b17619c3a80fc69a16392ec7fbef9974b913128
Edinson Aguado <edinsonaguado10@gmail.com>
2025-04-05 18:17:46 -0300

    Agregue un boton para volver a la ruta anterior segun el historial

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import './ProductDetail.css';
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -6,10 +6,12 @@ import { env } from "../../config/env.config";
 import Swal from "sweetalert2";
 import Loading from "../../components/Loading/Loading";
 import { useOrder } from "../../context/OrderContext";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function ProductDetail() {
     
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
     const {id} = useParams(); // Recibimos los datos del id.
     const [product, setProduct] = useState(null);
 
@@ -38,80 +40,86 @@ export default function ProductDetail() {
     }
 
     return (
-        <div className="main-container-detail-product">
+        <>
+            
+            <div className="main-container-detail-product">
+                <button className="btn-back" onClick={() => navigate(-1)}>
+                    <FontAwesomeIcon icon={faArrowLeft} className="icon-back"/>
+                    Volver
+                </button>
+                <div className="part-1">
+                    {/* IMAGE */}
+                    <div className="image-product">
+                        <img src={product?.image} alt={product?.name} />
+                    </div>
+                    <div className="detail-product">
+                        <div className="about-product-buy">
+                            
+                            {/* CATEGORY */}
+                            <div className="category">
+                                <span>{product?.category ?? 'Categoria no disponible'}</span>
+                            </div>
+                            {/* NAME */}
+                            <h3>{product?.name}</h3>
+                            {/* PRICE */}
+                            <div className="price">
+                                <p>$ {product?.price}</p>
+                            </div>
+                            {/* DESCRIPTION */}
+                            <div className="about-product">
+                                <p>{product?.description}</p>
+                            </div>
+                            {/* ACTIONS */}
+                            <div className="add-info">
+                                <form action="">
+                                    <label htmlFor="cantidadOrder">Cantidad</label>
+                                    <input
+                                        type="number"
+                                        id="cantidadOrder"
+                                        name="cantidadOrder"
+                                        min="1"
+                                        max="36"
+                                        step="1"
+                                        value={cantidadOrder}
+                                        onChange={(e) => setCantidad(Number(e.target.value))}
+                                    />
+                                    <div className="btns-buy">
+                                        <button 
+                                            className="add-btn" 
+                                            type="button"
+                                            onClick={() => addProduct(product)}>
+                                            Añadir al carrito
+                                        </button>
 
-            <div className="part-1">
-                {/* IMAGE */}
-                <div className="image-product">
-                    <img src={product?.image} alt={product?.name} />
-                </div>
-                <div className="detail-product">
-                    <div className="about-product-buy">
-                        
-                        {/* CATEGORY */}
-                        <div className="category">
-                            <span>{product?.category ?? 'Categoria no disponible'}</span>
-                        </div>
-                        {/* NAME */}
-                        <h3>{product?.name}</h3>
-                        {/* PRICE */}
-                        <div className="price">
-                            <p>$ {product?.price}</p>
-                        </div>
-                        {/* DESCRIPTION */}
-                        <div className="about-product">
-                            <p>{product?.description}</p>
-                        </div>
-                        {/* ACTIONS */}
-                        <div className="add-info">
-                            <form action="">
-                                <label htmlFor="cantidadOrder">Cantidad</label>
-                                <input
-                                    type="number"
-                                    id="cantidadOrder"
-                                    name="cantidadOrder"
-                                    min="1"
-                                    max="36"
-                                    step="1"
-                                    value={cantidadOrder}
-                                    onChange={(e) => setCantidad(Number(e.target.value))}
-                                />
-                                <div className="btns-buy">
-                                    <button 
-                                        className="add-btn" 
-                                        type="button"
-                                        onClick={() => addProduct(product)}>
-                                        Añadir al carrito
-                                    </button>
-
-                                    <button type="submit"
-                                    >
-                                        Comprar ahora
-                                    </button>
-                                </div>
-                            </form>
+                                        <button type="button"
+                                        >
+                                            Comprar ahora
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            {/* PRODUCT DETAILS */}
-            <div className="description-product">
-                <h3>Detalles de {product?.name}</h3>
-                <p>
-                    {product?.description ?? 'Descripción del producto no disponible.'}
-                </p>
-                <div className="datasheet">
+                {/* PRODUCT DETAILS */}
+                <div className="description-product">
+                    <h3>Detalles de {product?.name}</h3>
+                    <p>
+                        {product?.description ?? 'Descripción del producto no disponible.'}
+                    </p>
+                    <div className="datasheet">
 
-                    <h3>SITIO OFICIAL</h3>
-                    <a href={"https://www.ezviz.com"} target="_blank" rel="noopener noreferrer">
-                        Datasheet oficial
-                    </a>
-                    {/* NOOPENER: Evita que la nueva página tenga acceso a la propiedad window.opener, mejorando la seguridad y el rendimiento. */}
+                        <h3>SITIO OFICIAL</h3>
+                        <a href={"https://www.ezviz.com"} target="_blank" rel="noopener noreferrer">
+                            Datasheet oficial
+                        </a>
+                        {/* NOOPENER: Evita que la nueva página tenga acceso a la propiedad window.opener, mejorando la seguridad y el rendimiento. */}
 
-                    {/* NOOREFERRER: Evite que la nueva página reciba información sobre la página de origen. */}
+                        {/* NOOREFERRER: Evite que la nueva página reciba información sobre la página de origen. */}
 
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
